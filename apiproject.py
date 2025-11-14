f29c28e6b24b26acef0a383410cb6d2c580423c0
xiyangab <xiyangh@nycstudents.net>
2025-11-14 09:19:58 -0500

    uuyuuy

---
+++ apiproject.py
@@ -0,0 +1,18 @@
+import requests
+
+def getPoke(poke):
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+
+pokemon = getPoke("Bulbasaur")
+print(pokemon)
